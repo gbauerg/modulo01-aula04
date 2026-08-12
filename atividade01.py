e13d6d79703ea104c97bfdfee9b23f322a11c8cf
gbauerg <gabriel.delgado@aluno.senac.rio>
2026-08-12 18:24:17 -0300

    Atividade feita em sala.

---
+++ atividade01.py
@@ -0,0 +1,9 @@
+#calcular media bimestral dos alunos (teste e prova)
+#criar algoritmo que solicite as duas notas, calcule a media e de o resultado final
+
+nota_teste = float(input("Qual a nota do teste? "))
+nota_prova = float(input("Qual a nota da prova? "))
+
+calc_media = float(nota_teste + nota_prova)/2
+
+print(f"A média bimestral do aluno é {calc_media}.")
